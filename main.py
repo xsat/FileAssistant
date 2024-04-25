94a9c8fca64940e7b0aa8e97af5ff53dd751dd33
Yurii Antonchyk <xsat.php@gmail.com>
2024-04-25 12:53:14 +0300

    Add code types

--- main.py
+++ main.py
@@ -2,37 +2,32 @@ from tkinter import Tk, Frame, Label, Button, filedialog
 from os import walk
 
 
-def merge(dict1, dict2):
-    print(dict1, dict2)
-    return {**dict1, **dict2}
-
-
-def test():
-    tree = {}
-    directory = filedialog.askdirectory()
+def test() -> None:
+    tree: dict = {}
+    directory: str = filedialog.askdirectory()
 
     for (directory_path, directories, files) in walk(directory):
-        prev = tree
+        prev: dict = tree
 
         for inner_directory in directory_path.replace("\\", "/").split("/"):
             if inner_directory not in tree:
-                tree[inner_directory] = {}
+                tree[inner_directory]: dict = {}
 
-            tree = tree[inner_directory]
+            tree: dict = tree[inner_directory]
 
         for file in files:
             tree[file] = file
 
         # tree["__files__"] = files
 
-        tree = prev
+        tree: dict = prev
 
     print(tree)
 
 
 if __name__ == "__main__":
-    root = Tk()
-    frm = Frame(root)
+    root: Tk = Tk()
+    frm: Frame = Frame(root)
     frm.grid()
 
     Label(frm, text="Hello World!").grid(column=0, row=0)
